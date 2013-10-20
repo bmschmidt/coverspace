cfadd28eb77684111a3a0ebf08bcf309058e11d8
Benjamin Schmidt <bmschmidt@gmail.com>
2013-10-20 19:33:59 -0400

    Automatically creates thumbnails

--- imageConvert.py
+++ imageConvert.py
@@ -14,17 +14,35 @@ imageDirectory = "images"
 
 class myFile:
     def __init__(self,filename="1987-01-01.jpg"):
-        self.statistics = {"filename":imageDirectory +"/" + filename}
+        self.statistics = {"filename":imageDirectory +"/" + filename,"id":filename,"thumbnail":"thumbnails/" + filename}
         try:
             self.img = Image(filename=self.statistics['filename'])
+        except:
+            sys.stderr.write("Warning: imageMagick couldn't load " + self.statistics["filename"]+ "\n")
+            pass
+
+
+    def writeThumbnails(self):
+        try:
+            with self.img.clone() as i:
+                #Resize so the largest dimension is 100px wide.
+                if i.height > i.width and i.height > 100:
+                    i.transform(resize='x100')
+                elif i.width > 100:
+                    i.transform(resize='100x')
+                else:
+                    pass
+
+                self.thumbnail = i.clone()
+                i.save(filename=self.statistics["thumbnail"])
         except:
             pass
             
     def averages(self):
+        self.writeThumbnails()
         try:
-            img = self.img
+            img = self.thumbnail
         except:
-            sys.stderr.write("Warning: imageMagick couldn't load " + self.statistics["filename"]+ "\n")
             return dict()
         img.resize(1,1)
         average = img[0,0]
