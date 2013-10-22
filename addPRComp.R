259001cfdab9af778376590cc1bd54d084a1d861
Benjamin Schmidt <bmschmidt@gmail.com>
2013-10-22 08:51:49 -0400

    PRComp scripts

--- addPRComp.R
+++ addPRComp.R
@@ -2,7 +2,9 @@
 library(rjson)
 toSort = fromJSON( file = "summaries.json" )
 bound = do.call(rbind,lapply(toSort,as.data.frame))
-bound = cbind(bound,predict(prcomp(as.matrix(bound[,names(bound)!="filename"])))[,c(1:5)])
+matrix = as.matrix(bound[,names(bound)!="filename"])
+matrix = apply(matrix,2,rank)
+bound = cbind(bound,predict(prcomp(matrix)))[,c(1:5)])
 if (nrow(bound)==length(toSort)) {
   output = apply(bound,1,as.list)
   sapply(output,return)
